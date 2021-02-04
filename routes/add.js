64577be3064fe791464233cbdede8d2fd0c82582
ctakayama <cole.takayama@gmail.com>
2021-02-04 10:02:49 -0800

    fixed images

--- routes/add.js
+++ routes/add.js
@@ -3,10 +3,11 @@ var data = require("../data.json");
 exports.addFriend = function(request, response) {  
 	// Your code goes here
 	//add friend
+	var num = Math.floor(Math.random() * 100);
 	newFriend = {
 		"name": request.query.name,
 		"description": request.query.description,
-		"imageURL": "http://lorempixel.com/400/400/people"
+		"imageURL": "https://loremflickr.com/400/400/people?random=" + num
 	}
 	console.log(newFriend);
 	data.friends.push(newFriend);
